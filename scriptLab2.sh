90b1cd59502fc16f4604df9a093751118195fb1e
Divaiber <sasha.zelinsky02@gmail.com>
2023-03-22 13:19:37 +0200

    Initial commit

---
+++ scriptLab2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd $1
+
+direcroty_path=$1
+
+#Отримання шляху до локального каталогу та віддаленого репозиторію
+remote_repository=$2
+
+#Ініціалізація локального репозиторію
+git init
+#Додавання віддаленого репозиторію
+git remote add origin $remote_repository
+
+git branch -M main
+
+#Додавання всіх файлів у локальний репозиторій
+git add .
+
+#Створення коміту
+git commit -m "Initial commit"
+
+#Оновлення віддаленого репозиторію
+git push -u origin main
+
+echo "Script done"
